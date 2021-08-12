b106786b2d63df724bb8796146814ca744e631db
douglasmatosdev <douglasmatosdev@gmail.com>
2021-08-12 20:17:50 -0300

    create door model and add typescript

--- doors/pages/index.jsx
+++ doors/pages/index.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Door from "../components/Door";
 import Gift from "../components/Gift";
+import DoorModel from "../model/doorModel";
 
 export default function Home() {
+  const [d1, setD1] = useState(new DoorModel(1))
+
   return (
     <div style={{ display: 'flex' }}>
-      <Door />
-      <Door selected />
+      <Door door={d1} />
     </div>
   )
 }
