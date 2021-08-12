a42ebf86872ed47d288e0530605ee8f9738c68ae
douglasmatosdev <douglasmatosdev@gmail.com>
2021-08-12 19:44:38 -0300

    Draw door and gift

---
+++ doors/components/Door.jsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Door.module.css'
+
+export default function Door(props) {
+    return (
+        <div className={styles.area}>
+            <div className={styles.structure}>
+                <div className={styles.door}>
+                    <div className={styles.number}>3</div>
+                    <div className={styles.doorknob}></div>
+                </div>
+            </div>
+            <div className={styles.floor}></div>
+        </div>
+    )
+}
---
+++ doors/pages/index.jsx
@@ -0,0 +1,11 @@
+import Door from "../components/Door";
+import Gift from "../components/Gift";
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex' }}>
+      <Door />
+      <Door />
+    </div>
+  )
+}
